e14393046dd77ff9f1ded6ebd1970b8eeabb01e8
michalinab <michalina.bartosik@gmail.com>
2016-07-29 18:10:27 +0200

    pokemon attack last changes (small change)

--- src/main/java/it/morfoza/java101/pokemon/Main.java
+++ src/main/java/it/morfoza/java101/pokemon/Main.java
@@ -4,27 +4,27 @@ package it.morfoza.java101.pokemon;
 public class Main {
 
     public static void main(String[] args) {
-        Pokemon pokemon = new Pokemon("Pikachu", 1000, 100);
+        Pokemon pokemon1 = new Pokemon("Pikachu", 1000, 100);
         Pokemon pokemon2 = new Pokemon("Charizard", 500, 200);
         Pokemon pokemon3 = new Pokemon("Bulbasuar", 200, 300);
 
-        System.out.println(pokemon);
+        System.out.println(pokemon1);
         System.out.println(pokemon2);
         System.out.println(pokemon3);
 
-        pokemon2.attack(pokemon);
+        pokemon2.attack(pokemon1);
 
 
         System.out.println("Pokemony po walce");
-        System.out.println(pokemon);
+        System.out.println(pokemon1);
         System.out.println(pokemon2);
         System.out.println(pokemon3);
 
         System.out.println("Czy Pokemon żyje?");
-        if (pokemon.isAlive()) {
-            System.out.println(pokemon.toString() + " is alive");
+        if (pokemon1.isAlive()) {
+            System.out.println(pokemon1.toString() + " is alive");
         } else {
-            System.out.println(pokemon.toString() + " is unconscious");
+            System.out.println(pokemon1.toString() + " is unconscious");
         }
 
 
